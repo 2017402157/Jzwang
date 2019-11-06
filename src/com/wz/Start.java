505cd99ce0f8ceb82e6905f72753c8250ff3f802
lyp <1753190119@qq.com>
2019-11-06 21:50:28 +0800

    李颖鹏

--- src/com/wz/Start.java
+++ src/com/wz/Start.java
@@ -3,7 +3,9 @@ package com.wz;
 import com.jfinal.core.JFinal;
 
 public class Start {
-
+	/*
+	 * Ö÷³ÌÐò
+	 */
 	public static void main(String[] args) {
 		JFinal.start("WebContent", 8086, "/", 5);
 	}
