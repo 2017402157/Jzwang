573662004f861c19cbfeb78cc7ff77acaf8bab2e
12444 <12444@GP72mVR>
2019-11-18 13:18:15 +0800

    页面跳转

--- src/com/wz/controll/JzControl.java
+++ src/com/wz/controll/JzControl.java
@@ -18,12 +18,37 @@ public class JzControl extends Controller{
 	public void index() {
 		render("index.html");
 	}
+	
+	/**
+	 * 招聘首页
+	 */
+	public void recruit() {
+		render("page/recruit/index.htm");
+	}
+	
+	/**
+	 * 活动剪影首页
+	 */
+	public void photos() {
+		render("page/photos/index.htm");
+	}
+	
+	/**
+	 * 服务项目首页
+	 */
+	public void service() {
+		render("page/service/index.htm");
+	}
+	
+	/**
+	
 	/**
 	 * 新闻首页
 	 */
 	public void news() {
 		render("page/news/index.htm");
 	}
+	
 	/**
 	 * 获取新闻数据
 	 */
@@ -53,6 +78,12 @@ public class JzControl extends Controller{
 	}
 	public void test() {
 		render("page/news/test.html");
+	}
+	/**
+	 * 关于我们首页
+	 */
+	public void contact() {
+		render("page/contact/index.htm");
 	}
 
 }
