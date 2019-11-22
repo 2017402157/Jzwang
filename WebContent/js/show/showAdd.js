e69dc0798eb1bc413a88c281c85c1c13aad96d10
lyp <1753190119@qq.com>
2019-11-22 14:56:17 +0800

    lyp

--- WebContent/js/show/showAdd.js
+++ WebContent/js/show/showAdd.js
@@ -1,12 +1,13 @@
 var $;
 layui.config({
 	base : "js/"
-}).use(['form','layer','jquery', 'laydate'],function(){
+}).use(['form','layer','jquery', 'laydate', 'upload'],function(){
 	var form = layui.form,
 		layer = parent.layer === undefined ? layui.layer : parent.layer,
 		laypage = layui.laypage;
 		$ = layui.jquery
-		,laydate = layui.laydate;
+		,laydate = layui.laydate
+		,upload = layui.upload;
 	  
 	  //日期
 	  laydate.render({
@@ -17,7 +18,37 @@ layui.config({
 		    elem: '#data'
 		  });
 		
-		
+	  var uploadInst = upload.render({
+		    elem: '#upschool'
+		    ,url: '../upload/uploadImg'
+		    ,multiple:false
+		    ,before: function(obj){
+		      //预读本地文件示例，不支持ie8
+		      obj.preview(function(index, file, result){
+		    	  $('#demo1').attr('src', result); //图片链接（base64）
+		    	  $('#demoText').append('<a class="layui-upload-img" id="upload_img_'+index+'"><span><i class="iconfont">&#xe601;</i></span><img src="'+ result +'" alt="'+ file.name +'"></a>');
+		      });
+		    }
+		    ,done: function(res){
+		      //如果上传失败
+		      if(res.code == 0){
+		    	  $('#img').val(res.data.src);
+		        
+		      }else{
+		    	  return layer.msg('上传失败:'+res.msg);
+		      }
+		      //上传成功
+		    }
+		    ,error: function(){
+		      //演示失败状态，并实现重传
+		      var demoText = $('#demoText');
+		      demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
+		      demoText.find('.demo-reload').on('click', function(){
+		        uploadInst.upload();
+		      });
+		    }
+		  });
+	  
  	form.on("submit(add)",function(data){
  		var index;
  		 $.ajax({//异步请求返回给后台
--- src/com/wz/controll/UploadsConteroll.java
+++ src/com/wz/controll/UploadsConteroll.java
@@ -9,7 +9,7 @@ import com.wz.util.StringUtil;
 
 public class UploadsConteroll extends Controller {
 	
-	public void uploadImg() {
+	public void uploadNewsImg() {
 		UploadFile upFile = getFile();//单个上传文件一句搞定  默认路径是 upload
 		File file = upFile.getFile();
         String extName = StringUtil.getFileExt(file.getName());
@@ -27,6 +27,23 @@ public class UploadsConteroll extends Controller {
 		renderJson();
 	} 
 	
+	public void uploadImg() {
+		UploadFile upFile = getFile();//单个上传文件一句搞定  默认路径是 upload
+		File file = upFile.getFile();
+        String extName = StringUtil.getFileExt(file.getName());
+        String filePath = upFile.getUploadPath();
+        String fileName = System.currentTimeMillis() + extName;
+        file.renameTo(new File(filePath+"\\"+fileName));
+        
+        JSONObject json = new JSONObject();
+        json.put("src", "\\upload\\"+fileName);
+        json.put("title", fileName);
+        
+        setAttr("msg", "上传成功");
+		setAttr("code", 0);
+		setAttr("data", json);
+		renderJson();
+	}
 	
 
 }
