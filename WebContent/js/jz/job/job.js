f01fd49ba9c0839c8c3b3b80d5267c0cdc88c8ea
12444 <12444@172.16.1.139>
2019-12-17 14:38:21 +0800

    1

--- WebContent/js/jz/job/job.js
+++ WebContent/js/jz/job/job.js
@@ -19,7 +19,38 @@ layui.config({
 		form.render();
 	});
 		
-	  
+	var uploadInst = upload.render({
+	    elem: '#upschool'
+	    ,url: '../upload/uploadImg'
+	    ,multiple:false
+	    ,before: function(obj){
+	      //预读本地文件示例，不支持ie8
+	      obj.preview(function(index, file, result){
+	    	  $('#demo1').attr('src', result); //图片链接（base64）
+	    	  $('#demoText').append('<a class="layui-upload-img" id="upload_img_'+index+'"><span><i class="iconfont">&#xe601;</i></span><img style="width:500px;height:300px;" src="'+ result +'" alt="'+ file.name +'"></a>');
+	      });
+	    }
+	    ,done: function(res){
+	      //如果上传失败
+	      if(res.code == 0){
+	    	  $('#img').val(res.data.src);
+	        
+	      }else{
+	    	  return layer.msg('上传失败:'+res.msg);
+	      }
+	      //上传成功
+	    }
+	    ,error: function(){
+	      //演示失败状态，并实现重传
+	      var demoText = $('#demoText');
+	      demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
+	      demoText.find('.demo-reload').on('click', function(){
+	        uploadInst.upload();
+	      });
+	    }
+	  });
+	
+	
 	 	form.on("submit(jobadd)",function(data){
 	 		console.log(data.field);
 	 		 $.ajax({//异步请求返回给后台
